b0f4bc3ce99e1c3a6556e7c091f38ed2de1a6075
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-06-08 20:31:03 +0300

    add product controller

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv");
 const { path } = require("./app");
 
 //setting up config file
-dotenv.config({ path: "../backend/config/config.env" });
+dotenv.config({ path: "backend/config/config.env" });
 
 app.listen(process.env.PORT, () => {
   console.log(
