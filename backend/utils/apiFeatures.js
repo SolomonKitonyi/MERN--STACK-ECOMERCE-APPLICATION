9aa3412622954a3ed48e0cb1375df868b99bf5af
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-06-14 11:13:17 +0300

    advance filter

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -21,7 +21,12 @@ class APIFEATURES {
     //Removing some fields from the query
     const removeFields = ["keyword", "limit", "page"];
     removeFields.forEach((el) => delete queryCopy[el]);
-    this.query = this.query.find(queryCopy);
+
+    //Advance filter for price, rating etc
+    let queryStr = JSON.stringify(queryCopy);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);
+
+    this.query = this.query.find(JSON.parse(queryStr));
     return this;
   }
 }
