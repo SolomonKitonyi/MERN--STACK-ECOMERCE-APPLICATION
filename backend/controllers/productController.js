1cb16da91ff649cab30a2666e6116d74479eb84f
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-06-14 10:29:38 +0300

    add search controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -15,7 +15,9 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
 
 //Get all products => /api/v1/products
 exports.getProducts = catchAsyncErrors(async (req, res, next) => {
-  const apiFeatures = new APIFEATURES(Product.find(), req.query).search();
+  const apiFeatures = new APIFEATURES(Product.find(), req.query)
+    .search()
+    .filter();
   const products = await apiFeatures.query;
   res.status(200).json({
     success: true,
