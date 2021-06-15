14762e8881894f0cbfb1b76853c781c292448968
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-06-15 13:27:44 +0300

    add logout

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -44,3 +44,15 @@ exports.loginUser = catchAsyncError(async (req, res, next) => {
   }
   sendToken(user, 200, res);
 });
+
+//Logout user => /api/v1/logout
+exports.logout = catchAsyncError(async (req, res, next) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
+  res.status(200).json({
+    success: true,
+    message: "logged out",
+  });
+});
