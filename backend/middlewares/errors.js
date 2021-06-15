4070150ddf6e72f8f41d0f13d5b3c0089ca809a1
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-06-15 16:38:19 +0300

    handle jwt token errors

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -34,6 +34,19 @@ module.exports = (err, req, res, next) => {
       const message = `Duplicate ${Object.keys(err.keyValue)} entered.`;
       error = new ErrorHandler(message, 400);
     }
+
+    //Handling wrong JWT token error
+    if (err.name === "JsonWebTokenError") {
+      const message = "Json web token is invalid. Try again!!!";
+      error = new ErrorHandler(message, 400);
+    }
+
+    //Handling Expired JWT token error
+    if (err.name === "TokenExpiredError") {
+      const message = "Json web token is expired. Try again";
+      error = new ErrorHandler(message, 400);
+    }
+
     res.status(err.statusCode).json({
       success: false,
       message: error.message || "Internal Server Error",
