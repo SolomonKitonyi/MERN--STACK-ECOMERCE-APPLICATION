34307cf4de527473c77576124d4b844fa2c17338
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-06-15 13:50:30 +0300

    add role based authorization

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -15,3 +15,18 @@ exports.isAuthentictedUser = catchAsyncError(async (req, res, next) => {
   req.user = await User.findById(decoded.id);
   next();
 });
+
+//Handle roles
+exports.authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorHandler(
+          `Role (${req.user.role}) is not allowed to access this resource`,
+          403
+        )
+      );
+    }
+    next();
+  };
+};
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -8,14 +8,16 @@ const {
   updateProduct,
   deleteProduct,
 } = require("../controllers/productController");
-const { isAuthentictedUser } = require("../middlewares/auth");
+const { isAuthentictedUser, authorizeRoles } = require("../middlewares/auth");
 
 router.route("/products").get(getProducts);
 router.route("/product/:id").get(getSingleProduct);
-router.route("/admin/product/new").post(isAuthentictedUser, newProduct);
+router
+  .route("/admin/product/new")
+  .post(isAuthentictedUser, authorizeRoles("admin"), newProduct);
 router
   .route("/admin/product/:id")
-  .put(isAuthentictedUser, updateProduct)
-  .delete(isAuthentictedUser, deleteProduct);
+  .put(isAuthentictedUser, authorizeRoles("admin"), updateProduct)
+  .delete(isAuthentictedUser, authorizeRoles("admin"), deleteProduct);
 
 module.exports = router;
