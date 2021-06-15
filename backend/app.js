977d3f133539a8a8ee56dc9214d43306ae2befab
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-06-15 13:09:12 +0300

    fix jwt expiration bug

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 const errorMiddleware = require("./middlewares/errors");
 
 app.use(express.json());
+app.use(cookieParser());
 //import all routes
 const products = require("./routes/product");
 const auth = require("./routes/auth");
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -8,8 +8,9 @@ const {
   updateProduct,
   deleteProduct,
 } = require("../controllers/productController");
+const { isAuthentictedUser } = require("../middlewares/auth");
 
-router.route("/products").get(getProducts);
+router.route("/products").get(isAuthentictedUser, getProducts);
 router.route("/product/:id").get(getSingleProduct);
 router.route("/admin/product/new").post(newProduct);
 router.route("/admin/product/:id").put(updateProduct).delete(deleteProduct);
