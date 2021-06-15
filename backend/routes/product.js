9e480e23f7a225eef1b799c9ba54c58310a4bd8c
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-06-15 13:29:25 +0300

    add authenticated routes

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -10,9 +10,12 @@ const {
 } = require("../controllers/productController");
 const { isAuthentictedUser } = require("../middlewares/auth");
 
-router.route("/products").get(isAuthentictedUser, getProducts);
+router.route("/products").get(getProducts);
 router.route("/product/:id").get(getSingleProduct);
-router.route("/admin/product/new").post(newProduct);
-router.route("/admin/product/:id").put(updateProduct).delete(deleteProduct);
+router.route("/admin/product/new").post(isAuthentictedUser, newProduct);
+router
+  .route("/admin/product/:id")
+  .put(isAuthentictedUser, updateProduct)
+  .delete(isAuthentictedUser, deleteProduct);
 
 module.exports = router;
