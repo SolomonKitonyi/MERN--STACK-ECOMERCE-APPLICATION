d1092ace2e3d12e48d273091c37f4fae79eff608
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-06-16 11:00:06 +0300

    get user profile

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
+const { isAuthentictedUser } = require("../middlewares/auth");
+
 const {
   registerUser,
   loginUser,
   logout,
   forgotPassward,
   resetPassword,
+  getUserProfile,
 } = require("../controllers/authController");
 
 router.route("/register").post(registerUser);
@@ -14,4 +17,5 @@ router.route("/login").post(loginUser);
 router.route("/password/forgot").post(forgotPassward);
 router.route("/password/reset/:token").put(resetPassword);
 router.route("/logout").get(logout);
+router.route("/me").get(isAuthentictedUser, getUserProfile);
 module.exports = router;
