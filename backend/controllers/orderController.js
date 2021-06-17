1731d9f531b7f2b3dbfe97c4ba8da5f82a8d34ef
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-06-17 15:29:36 +0300

    add get single and logged in user orders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -3,6 +3,7 @@ const Product = require("../models/product");
 
 const ErrrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middlewares/catchAsyncError");
+const ErrorHandler = require("../utils/errorHandler");
 
 //Create new order => /api/v1/order/new
 exports.newOrder = catchAsyncErrors(async (req, res, next) => {
@@ -29,5 +30,32 @@ exports.newOrder = catchAsyncErrors(async (req, res, next) => {
   res.status(200).json({
     success: true,
     order,
+  });
+});
+
+//Get single order => /api/v1/order/:id
+exports.getSingleOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (!order) {
+    return next(new ErrorHandler("No order found with this ID", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
+
+//Get orders for logged in user => /api/v1/orders/me
+exports.myOrders = catchAsyncErrors(async (req, res, next) => {
+  const orders = await Order.find({ user: req.user.id });
+
+  res.status(200).json({
+    success: true,
+    orders,
   });
 });
