a906ea11865baa4a09e3257f9d459abdfb9bd0cb
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-06-17 15:30:02 +0300

    add get single and logged in user routes

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const { newOrder } = require("../controllers/orderController");
+const {
+  newOrder,
+  getSingleOrder,
+  myOrders,
+} = require("../controllers/orderController");
 const { isAuthentictedUser, authorizeRoles } = require("../middlewares/auth");
 
 router.route("/order/new").post(isAuthentictedUser, newOrder);
+router.route("/order/:id").get(isAuthentictedUser, getSingleOrder);
+router.route("/orders/me").get(isAuthentictedUser, myOrders);
 
 module.exports = router;
