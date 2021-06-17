1795b4f0b983aeeb2361cd153d5bf9e5d5143743
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-06-17 17:54:31 +0300

    add update order admin

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -5,11 +5,20 @@ const {
   newOrder,
   getSingleOrder,
   myOrders,
+  allOrders,
+  updateOrder,
 } = require("../controllers/orderController");
 const { isAuthentictedUser, authorizeRoles } = require("../middlewares/auth");
 
 router.route("/order/new").post(isAuthentictedUser, newOrder);
 router.route("/order/:id").get(isAuthentictedUser, getSingleOrder);
 router.route("/orders/me").get(isAuthentictedUser, myOrders);
+router
+  .route("/admin/orders")
+  .get(isAuthentictedUser, authorizeRoles("admin"), allOrders);
+
+router
+  .route("/admin/order/:id")
+  .put(isAuthentictedUser, authorizeRoles("admin"), updateOrder);
 
 module.exports = router;
