8d95600f7ae26386e093260803fec112c0cb5a6b
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-06-21 14:46:24 +0300

    add react-alert

--- fronted/src/index.js
+++ fronted/src/index.js
@@ -1,13 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { positions, transitions, Provider as Alertprovider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
 
 import store from "./store";
 import App from "./App";
 
+const options = {
+  timeout: 5000,
+  position: positions.BOTTOM_CENTER,
+  transition: transitions.SCALE,
+};
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Alertprovider template={AlertTemplate} {...options}>
+      <App />
+    </Alertprovider>
   </Provider>,
   document.getElementById("root")
 );
