b1e6776b305e242d1c3d8af48535123facf799cf
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-06-22 12:30:20 +0300

    add link to home

--- fronted/src/components/Home.js
+++ fronted/src/components/Home.js
@@ -2,14 +2,20 @@ import React, { Fragment, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useAlert } from "react-alert";
 import Pagination from "react-js-pagination";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 
 import MetaData from "./layout/MetaData";
 import Product from "./product/Product";
 import Loader from "./layout/Loader";
 import { getProducts } from "../actions/productActions";
 
+const { createSliderWithTooltip } = Slider;
+const Range = createSliderWithTooltip(Slider.Range);
+
 const Home = ({ match }) => {
   const [currentPage, setCurrentPage] = useState(1);
+  const [price, setPrice] = useState([50, 100000]);
   const alert = useAlert();
   const dispatch = useDispatch();
 
@@ -22,8 +28,8 @@ const Home = ({ match }) => {
     if (error) {
       return alert.error(error);
     }
-    dispatch(getProducts(keyword, currentPage));
-  }, [dispatch, alert, error, keyword, currentPage]);
+    dispatch(getProducts(keyword, currentPage, price));
+  }, [dispatch, alert, error, keyword, currentPage, price]);
 
   function setCurrentPageNo(pageNumber) {
     setCurrentPage(pageNumber);
@@ -40,10 +46,41 @@ const Home = ({ match }) => {
 
           <section id="products" className="container mt-5">
             <div className="row">
-              {products &&
+              {keyword ? (
+                <Fragment>
+                  <div className="col-6 col-md-3 mt-5 mb-5">
+                    <div className="px-5">
+                      <Range
+                        marks={{
+                          50: `Ksh50`,
+                          100000: `ksh100000`,
+                        }}
+                        min={50}
+                        max={100000}
+                        defaultValue={[50, 100000]}
+                        tipFormatter={(value) => `Ksh${value}`}
+                        tipProps={{
+                          placement: "top",
+                          visible: true,
+                        }}
+                        value={price}
+                        onChange={(price) => setPrice(price)}
+                      />
+                    </div>
+                  </div>
+                  <div className="col-6 col-md-9">
+                    <div className="row">
+                      {products.map((product) => (
+                        <Product key={product._id} product={product} col={4} />
+                      ))}
+                    </div>
+                  </div>
+                </Fragment>
+              ) : (
                 products.map((product) => (
-                  <Product key={product._id} product={product} />
-                ))}
+                  <Product key={product._id} product={product} col={3} />
+                ))
+              )}
             </div>
           </section>
           {resPerPage <= productsCount && (
