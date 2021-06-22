ec351254116fa1b82c4f404a1dbefb9d07e1732c
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-06-22 15:55:55 +0300

    add filteredProductsCount

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -21,13 +21,17 @@ exports.getProducts = catchAsyncErrors(async (req, res, next) => {
   const productCount = await Product.countDocuments();
   const apiFeatures = new APIFEATURES(Product.find(), req.query)
     .search()
-    .filter()
-    .pagination(resPerPage);
-  const products = await apiFeatures.query;
+    .filter();
+
+  let products = await apiFeatures.query;
+  let filteredProductsCount = products.length;
+  apiFeatures.pagination(resPerPage);
+  products = await apiFeatures.query;
   res.status(200).json({
     success: true,
     productCount,
     resPerPage,
+    filteredProductsCount,
     products,
   });
 });
