7c28b4bc7085ec32d00d2c8e0201e50bcc816c9e
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-06-23 14:02:41 +0300

    configure cloudinary

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const bodyparser = require("body-parser");
+const cloudinary = require("cloudinary");
 const app = express();
 const errorMiddleware = require("./middlewares/errors");
 
 app.use(express.json());
+app.use(bodyparse.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+//Setting up cloudinary configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_CLOUD_APIKEY,
+  api_secret: CLOUDINARY_CLOUD_SECRET,
+});
+
 //import all routes
 const products = require("./routes/product");
 const auth = require("./routes/auth");
