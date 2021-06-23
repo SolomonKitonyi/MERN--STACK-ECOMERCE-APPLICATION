ab49e195cca49b0a35ecf3d22ef7f56f62117b1f
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-06-23 12:07:51 +0300

    add auth

--- fronted/src/store.js
+++ fronted/src/store.js
@@ -6,10 +6,12 @@ import {
   productReducers,
   productDetailsReducer,
 } from "./reducers/productReducers";
+import { authReducer } from "./reducers/userReducers";
 
 const reducer = combineReducers({
   products: productReducers,
   productDetails: productDetailsReducer,
+  auth: authReducer,
 });
 
 let initialState = {};
