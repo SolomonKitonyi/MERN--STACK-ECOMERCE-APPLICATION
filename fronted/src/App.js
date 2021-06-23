ee83181a09db81f60c1b9acb0128bf04c9708b02
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-06-23 15:34:13 +0300

    add register

--- fronted/src/App.js
+++ fronted/src/App.js
@@ -6,6 +6,7 @@ import Footer from "./components/layout/Footer";
 import Home from "./components/Home";
 import ProductDetails from "./components/product/ProductDetails";
 import Login from "./components/user/Login";
+import Register from "./components/user/Register";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path="/search/:keyword" component={Home} />
           <Route path="/product/:id" component={ProductDetails} exact />
           <Route path="/login" component={Login} exact />
+          <Route path="/register" component={Register} exact />
         </div>
         <Footer />
       </div>
