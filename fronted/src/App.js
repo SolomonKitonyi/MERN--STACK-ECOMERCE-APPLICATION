134c753b46696cace550d8fd193b30324074a54f
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-06-25 16:00:10 +0300

    add profile route

--- fronted/src/App.js
+++ fronted/src/App.js
@@ -8,6 +8,7 @@ import Home from "./components/Home";
 import ProductDetails from "./components/product/ProductDetails";
 import Login from "./components/user/Login";
 import Register from "./components/user/Register";
+import Profile from "./components/user/Profile";
 import { loadUser } from "./actions/userActions";
 import Store from "./store";
 
@@ -25,6 +26,7 @@ function App() {
           <Route path="/product/:id" component={ProductDetails} exact />
           <Route path="/login" component={Login} exact />
           <Route path="/register" component={Register} exact />
+          <Route path="/me" component={Profile} exact />
         </div>
         <Footer />
       </div>
