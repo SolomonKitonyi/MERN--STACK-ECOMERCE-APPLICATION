9947d5310e0951a937819b465e8e1431cc490a4c
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-07-31 12:28:59 +0300

    add stripe api key route

--- backend/routes/payment.js
+++ backend/routes/payment.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { processPayment } = require("../controllers/paymentController");
+const {
+  processPayment,
+  sendStripeApi,
+} = require("../controllers/paymentController");
 const { isAuthentictedUser } = require("../middlewares/auth");
 
 router.route("/payment/process").post(isAuthentictedUser, processPayment);
+router.route("/stripeapi").get(isAuthentictedUser, sendStripeApi);
 
 module.exports = router;
